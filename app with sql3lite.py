938a8fb24176e495e6e3607c059a2d6e130797da
OlaDzido <aleksandra_dzido@wp.pl>
2020-02-28 15:01:26 +0100

    change 4

--- app with sql3lite.py
+++ app with sql3lite.py
@@ -11,3 +11,5 @@ a =(datetime.date.today())
 x = dt.strftime(a, '%Y_%m_%d')
 c.execute("""CREATE TABLE IF NOT EXISTS ranking{datax} (place INT(4), tittle VARCHAR(40), votes INT(20), data DATE);""".format(datax=x))
 c.commit()
+
+
--- app.py
+++ app.py
@@ -10,66 +10,100 @@ import datetime
 from datetime import date
 from datetime import datetime as dt
 
-url = 'https://www.filmweb.pl/ranking/netflix/Komedia/13/2018'
-page = requests.get(url)
-html_soup = BeautifulSoup(page.text, 'html.parser')
-movie_containers = html_soup.find_all('div', {'class': 'item place'})
 
-name = []
-ratings = []
-votes = []
-places = []
-day = date.today()
-
-for elem in movie_containers:
+a = (datetime.date.today())
+x = dt.strftime(a, '%Y_%m_%d')
 
 
-    title = elem.h3.a.text
-    if title not in name:
-        name.append(title)
+def scrapping ():
+    url = 'https://www.filmweb.pl/ranking/netflix/Komedia/13/2018'
+    page = requests.get(url)
+    html_soup = BeautifulSoup(page.text, 'html.parser')
+    movie_containers = html_soup.find_all('div', {'class': 'item place'})
 
-    rate = elem.find('span', {'class' : 'rate__value'}).text
-    if rate not in name:
-        ratings.append(rate)
+    name = []
+    ratings = []
+    votes = []
+    places = []
+    day = date.today()
 
-    vote = elem.find('span', {'class' : 'rate__count'}).text[:-6]
-    if vote not in votes:
-        votes.append(vote)
+    for elem in movie_containers:
 
-    place = elem.h3.span.text[:-1]
-    if place not in places:
-        places.append(place)
 
-votes = [elem.replace(" ", "") for elem in votes]
-votes = [int(elem) for elem in votes]
-ratings = [elem.replace(",",".") for elem in ratings]
-ratings = [(float(elem)) for elem in ratings]
-places = [int(elem) for elem in places]
-data = {'place': places,
+        title = elem.h3.a.text
+        if title not in name:
+            name.append(title)
+
+        rate = elem.find('span', {'class' : 'rate__value'}).text
+        if rate not in name:
+            ratings.append(rate)
+
+        vote = elem.find('span', {'class' : 'rate__count'}).text[:-6]
+        if vote not in votes:
+            votes.append(vote)
+
+        place = elem.h3.span.text[:-1]
+        if place not in places:
+            places.append(place)
+
+    votes = [elem.replace(" ", "") for elem in votes]
+    votes = [int(elem) for elem in votes]
+    ratings = [elem.replace(",",".") for elem in ratings]
+    ratings = [(float(elem)) for elem in ratings]
+    places = [int(elem) for elem in places]
+    data = {'place': places,
         'tittle': name,
         'ratings': ratings,
         'votes': votes,
         'data' : str(day)
         }
 
-df = pd.DataFrame(data, columns=['place', 'tittle','ratings','votes', 'data'])
+    df = pd.DataFrame(data, columns=['place', 'tittle','ratings','votes', 'data'])
 
-tuples=list(df.itertuples(index=False, name=None))
+    tuples=list(df.itertuples(index=False, name=None))
 
 
 
-connection = pymysql.connect(host='127.0.0.1',
-                             user='username',
-                             password='password',
-                             db='films',
-                             charset='utf8mb4')
 
-c = connection.cursor()
-a =(datetime.date.today())
-print(tuples)
-x = dt.strftime(a, '%Y_%m_%d')
-c.execute("""CREATE TABLE IF NOT EXISTS ranking{datax} (place INT(4) NOT NULL, tittle VARCHAR(60) NOT NULL, ratings DOUBLE NOT NULL, votes INT(20) NOT NULL, data DATE NOT NULL);""".format(datax=x, elem=elem))
-connection.commit()
-sql=("""INSERT INTO ranking{datax} (place,tittle,ratings,votes, data) VALUES (%s,%s,%s,%s,%s);""".format(datax=x,))
-c.executemany(sql,tuples)
-connection.commit()
+
+try:
+    connection = pymysql.connect(host='127.0.0.1',
+                                     user='username',
+                                     password='password',
+                                     db='films',
+                                     charset='utf8mb4')
+
+
+    print("Success")
+
+except Exception as e:
+        print (e)
+        print ("No connection with database")
+else:
+    c = connection.cursor()
+    c.execute("""SHOW TABLES FROM films;""")
+
+
+    result = c.fetchall()
+    format_result = []
+    for elem in result:
+        format_result.append((''.join(map(str, elem))))
+
+    for elem in format_result:
+        if elem == "ranking{datax}".format(datax =x):
+            print("Tabela z dnia {} już istnieje".format(elem))
+    else: scrapping()
+
+
+
+
+
+# c = self.connect.cursor()
+# a =(datetime.date.today())
+#
+# x = dt.strftime(a, '%Y_%m_%d')
+# c.execute("""CREATE TABLE IF NOT EXISTS ranking{datax} (place INT(4) NOT NULL, tittle VARCHAR(60) NOT NULL, ratings DOUBLE NOT NULL, votes INT(20) NOT NULL, data DATE NOT NULL);""".format(datax=x, elem=elem))
+# connection.commit()
+# # sql=("""INSERT INTO ranking{datax} (place,tittle,ratings,votes, data) VALUES (%s,%s,%s,%s,%s);""".format(datax=x,))
+# # c.executemany(sql,tuples)
+# # connection.commit()
